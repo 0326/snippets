9f76fa061ee55badfc68339dbf8c55032b053ea6
0326 <winniringy@gmail.com>
2017-08-16 20:59:18 +0800

    feat: jekyll version

---
+++ category/code-snippets.md
@@ -0,0 +1,6 @@
+---
+layout: posts_by_category
+categories: code-snippets
+title: 代码片段
+permalink: /category/code-snippets
+---
--- publish.sh
+++
@@ -1,5 +0,0 @@
-hugo --theme=material-design
-cd public
-git add --all
-git commit -m "update blog"
-git push -f origin gh-pages
